ff65417839a0a4a6d974ac748d9d1a0f86fb0d36
lfarssi <medlfarssi10@gmail.com>
2024-11-25 14:19:54 +0100

    final porject

--- routes/router.go
+++ routes/router.go
@@ -7,12 +7,9 @@ import (
 )
 
 func Router() {
-	// fs := http.FileServer(http.Dir("static"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
 	http.HandleFunc("/", controllers.ArtistController)
 	http.HandleFunc("/static/", controllers.StaticController)
 	http.HandleFunc("/artist/{id}", controllers.ArtistDetailController)
-	log.Println("Server running on http://localhost:8089")
-	log.Fatal(http.ListenAndServe(":8089", nil))
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
